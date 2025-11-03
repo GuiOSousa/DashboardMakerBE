3af484620f22af208922ffa4302ae7af90dd5262
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-11-03 14:38:44 -0300

    Security Hotfix

--- src/connection/mongoClient.ts
+++ src/connection/mongoClient.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import { env } from 'process';
 
-const uri = env.URI || "mongodb+srv://guiOS:yV4pAc3qk1uu44zZ@cluster0.kfckmci.mongodb.net/?appName=Cluster0"
+const uri = env.URI || ""
 
 export const mongoClient = new MongoClient(uri, {
     serverApi: {
